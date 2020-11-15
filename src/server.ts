ae573fdabe3511dd9a233a2d2ccf606e361a6f1c
Celso Alexandre <celsoalexandre@live.com>
2020-11-15 00:03:48 -0300

    Commit temporario

--- src/server.ts
+++ src/server.ts
@@ -9,14 +9,27 @@ server.use(express.json());
 
 server.post('/git/repository/create', async (request, response) => {
   const { repositoryName } = request.body;
-  const pathToRepo = path.resolve(`./tmp/repository/test/${repositoryName}`);
+  const pathToRepo = path.resolve(`./tmp/repository/${repositoryName}`);
 
   if (!fs.existsSync(pathToRepo)) {
     fs.mkdirSync(pathToRepo);
-    await git.Repository.init(pathToRepo, 1);
+    await git.Repository.init(pathToRepo, 0);
   }
 
-  await git.Repository.open(pathToRepo);
+  const repo = await git.Repository.open(pathToRepo);
+  let remotes = await git.Remote.list(repo) as string[];
+  remotes = remotes.filter((remote_) => (
+    remote_ === 'origin'
+  ));
+
+  if (remotes && remotes.length > 0) {
+    await git.Remote.delete(repo, 'origin');
+  }
+
+  git.Remote.create(repo, 'origin', 'https://github.com/celso-alexandre/gitgubapi-test.git');
+  git.Remote.addPush(repo, 'origin', 'master');
+  git.Remote.addFetch(repo, 'origin', 'master');
+  const fetch = new git.Fetch();
 
   return response.json({ ok: true });
 });
